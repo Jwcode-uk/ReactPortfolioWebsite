7d89af93bfb22cebe135774eb18cc078c851ea4b
Jwcode-uk <Jonathanwhite0000@gmail.com>
2020-06-11 02:26:40 +0100

    improved cards

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import "./App.scss";
 import "react-bulma-components/dist/react-bulma-components.min.css";
-import { Button } from "react-bulma-components";
 import { Card } from "./card.jsx";
-import { Modal } from "react-bulma-components";
 import CardContent from "./cardContent";
 import { useTransition, animated, useSpring } from "react-spring";
 
@@ -13,11 +11,11 @@ function App() {
 
   const loadContent = (val) => {
     set(false);
-    document.getElementById("content").scrollIntoView({ behavior: "smooth" });
 
     setTimeout(() => {
       setContext(val);
       set(true);
+      document.getElementById("content").scrollIntoView({ behavior: "smooth" });
     }, 200);
   };
   const props = useSpring({
@@ -37,25 +35,29 @@ function App() {
 
       <div class="container">
         <br />
-        <center>
-          <figure class="image is-128x128">
-            <img class="is-rounded" src={require("./img/profile.jpg")} />
-          </figure>
-          <div class="columns is-tablet">
-            <div class="column"></div>
-            <div class="column is-three-fifths">
-              <p class="title">About me</p>
-              <p>
-                Hi, I'm Jonathan, I'm a software developer currently studying in
-                2nd year at Newcastle University. I do freelance work in
-                Software Development and Youth Work as well as Charity Work when
-                I have chance. Feel Free to email me about any opportunitys you
-                have.
-              </p>
-            </div>
-            <div class="column"></div>
+        <div class="columns is-tablet">
+          <div class="column"></div>
+          <div class="column is-three-fifths">
+            <center>
+              <figure class="is-centered image is-128x128 is-centered">
+                <img
+                  class="is-rounded is-centered"
+                  src={require("./img/profile.jpg")}
+                />
+              </figure>
+              <br />
+            </center>
+
+            <p class="title">About me</p>
+            <p>
+              Hi, I'm Jonathan, I'm a software developer currently studying in
+              2nd year at Newcastle University. I do freelance work in Software
+              Development and Youth Work as well as Charity Work when I have
+              chance. Feel Free to email me about any opportunitys you have.
+            </p>
           </div>
-        </center>
+          <div class="column"></div>
+        </div>
       </div>
       <br />
       <p class="title">My Work</p>
@@ -85,7 +87,7 @@ function App() {
           <Card
             contents="I represent my school at meetings pushing for improvements to our experiences and helping create adaption for COVID"
             title="School Rep"
-            imgSrc="cclogo.png"
+            imgSrc="meeting.png"
             imgAlt="this is a img"
             date="Active"
             tagType="is-success"
@@ -150,7 +152,7 @@ function App() {
               <animated.div style={props} id="content">
                 <div class="container" border-radius="25px">
                   <div
-                    id="rcorners"
+                    id="rounded"
                     class=" has-background-light"
                     border-radius="25px"
                   >
@@ -164,38 +166,6 @@ function App() {
         </div>
       </center>
       <br />
-
-      <footer class="footer">
-        <div class="content has-text-centered">
-          <p>Contact me and I&#39;ll get back to you within a day.</p>
-          <p>
-            <i class="fa fa-map"></i> Newcastle, England
-          </p>
-          <p>
-            <i class="fa fa-envelope"></i> jwcode@pm.me
-          </p>
-          <button
-            type="button"
-            onclick="location.href='https://github.com/Jwcode-uk/'"
-            class="btn btn-outline-dark"
-          >
-            GitHub
-          </button>
-          <button
-            type="button"
-            onclick="location.href='https://www.linkedin.com/in/jonathan-white-536353172/'"
-            class="btn btn-outline-dark"
-          >
-            LinkedIn
-          </button>
-          <p>
-            <i>
-              &copy; Jonathan White 2020 | Banner Image &copy; Oliver Butler
-              2020
-            </i>
-          </p>
-        </div>
-      </footer>
     </div>
   );
 }
--- my-app/src/card.jsx
+++ my-app/src/card.jsx
@@ -4,38 +4,37 @@ import "react-bulma-components/dist/react-bulma-components.min.css";
 
 export function Card(Props) {
   return (
-    <center>
-      <div class="cardContainer">
-        <div class="card">
-          {Props.imgSrc == "NA" ? (
-            <p></p>
-          ) : (
-            <div class="card-image">
-              <figure class="image is-4by3">
-                <img
-                  src={require("./img/" + Props.imgSrc)}
-                  alt={Props.imgAlt}
-                />
-              </figure>
-            </div>
-          )}
-
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="subtitle">
-                  {Props.title}
-                  <span class={"tag is-pulled-right " + Props.tagType}>
-                    {Props.date}
-                  </span>
-                </p>
-              </div>
-            </div>
-            <div class="content">{Props.contents}</div>
+    <div class="cardContainer ">
+      <div class="card">
+        {Props.imgSrc == "NA" ? (
+          <p></p>
+        ) : (
+          <div class="card-image">
+            <figure class=" rounded-corners image is-4by3">
+              <img
+                class="rounded-corners-top"
+                src={require("./img/" + Props.imgSrc)}
+                alt={Props.imgAlt}
+              />
+            </figure>
           </div>
+        )}
+
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="subtitle">
+                {Props.title}
+                <span class={"tag is-pulled-right " + Props.tagType}>
+                  {Props.date}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div class="content">{Props.contents}</div>
         </div>
       </div>
-    </center>
+    </div>
   );
 }
 
